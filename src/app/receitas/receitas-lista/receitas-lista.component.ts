38b10c47688048afc01d1f42edabe2511cbdb0cc
josealgeri <josealgeri@gmail.com>
2017-09-23 12:10:32 -0300

    Criação de componentes

--- src/app/receitas/receitas-lista/receitas-lista.component.ts
+++ src/app/receitas/receitas-lista/receitas-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Receitas} from "../receitas.model";
 
 @Component({
   selector: 'app-receitas-lista',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receitas-lista.component.css']
 })
 export class ReceitasListaComponent implements OnInit {
-  receitas = [];
+  receitas: Receitas[] = [
+    new Receitas('Receita teste', 'Teste de receita',
+      'http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+    new Receitas('Receita teste', 'Teste de receita',
+      'https://c1.staticflickr.com/6/5643/30102545481_54a2fdeeaa_b.jpg'),
+    new Receitas('Receita teste', 'Teste de receita',
+      'https://cdn.pixabay.com/photo/2015/07/05/21/38/salad-832811_960_720.jpg')
+
+  ];
+
+
 
   constructor() { }
 
--- src/app/shopping-lista/shopping-lista.component.ts
+++ src/app/shopping-lista/shopping-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingrediente} from "../shared/ingrediente.model";
 
 @Component({
   selector: 'app-shopping-lista',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-lista.component.css']
 })
 export class ShoppingListaComponent implements OnInit {
+  ingredientes: Ingrediente[] = [
+    new Ingrediente('Maçãs', 5 ),
+    new Ingrediente('Tomates', 10 )
+
+  ];
 
   constructor() { }
 
